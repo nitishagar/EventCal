4ccf50ce08d5e2594f31cc35e2b37367a103128b
Yuduo Zhang <yuduozhang@gmail.com>
2013-06-07 15:55:35 -0400

    dayView works. But it's not a functional one. I haven't tested with any event data.

--- src/cs/softwarearchitecture/eventcal/DefaultView.java
+++ src/cs/softwarearchitecture/eventcal/DefaultView.java
@@ -1,6 +1,9 @@
 package cs.softwarearchitecture.eventcal;
 
+import java.util.Calendar;
 import java.util.Locale;
+
+import com.viewpagerindicator.TitlePageIndicator;
 
 import cs.softwarearchitecture.eventcal.R;
 import cs.softwarearchitecture.eventcal.SettingsActivity;
@@ -11,13 +14,14 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class DefaultView extends FragmentActivity {
 
@@ -29,27 +33,136 @@ public class DefaultView extends FragmentActivity {
 	 * intensive, it may be best to switch to a
 	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
 	 */
-	SectionsPagerAdapter mSectionsPagerAdapter;
+	CalendarPagerAdapter mCalendarPagerAdapter;
+	int  PAGE_NUMBER = 3;
+	int CURRENT_POSITION = 1;
+	
+	int currentDay;
+	int currentMonth;
+	int currentYear;
+	
+	int previousDay;
+	int previousMonth;
+	int previousYear;
+	
+	int nextDay;
+	int nextMonth;
+	int nextYear;
+	
+	int currentPage;
+	
+	private String currentDate;
+	private String previousDate;
+	private String nextDate;
+	
+	
+	//Calendar calendar; 
+	Calendar calChanging;
 
+	
+	private String monthsName[] = {
+			"Jan", "Feb", "Mar", "Apr", "May", "Jun",
+			"Jul", "Aug", "Sep", "Oct", "Nov", "Dec" 	
+	};
+	
 	/**
 	 * The {@link ViewPager} that will host the section contents.
 	 */
-	ViewPager mViewPager;
+	ViewPager dayViewPager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_default_view);
 
+		
+		//calendar = Calendar.getInstance(Locale.getDefault());
+		calChanging = Calendar.getInstance(Locale.getDefault());
+		
+		updateDate(0);
+		
 		// Create the adapter that will return a fragment for each of the three
 		// primary sections of the app.
-		mSectionsPagerAdapter = new SectionsPagerAdapter(
+		mCalendarPagerAdapter = new CalendarPagerAdapter(
 				getSupportFragmentManager());
 
 		// Set up the ViewPager with the sections adapter.
-		mViewPager = (ViewPager) findViewById(R.id.pager);
-		mViewPager.setAdapter(mSectionsPagerAdapter);
+		dayViewPager = (ViewPager) findViewById(R.id.dayViewPager);
+		dayViewPager.setAdapter(mCalendarPagerAdapter);
+		
+		
+		final TitlePageIndicator pageIndicator = 
+				(TitlePageIndicator) findViewById(R.id.pageIndicator);
+		pageIndicator.setViewPager(dayViewPager,CURRENT_POSITION);
+		pageIndicator.setOnPageChangeListener(new OnPageChangeListener(){
 
+			@Override
+			public void onPageScrollStateChanged(int state) {
+				// TODO Auto-generated method stub
+				
+				if (state == ViewPager.SCROLL_STATE_IDLE) {	
+					if (currentPage < 1){
+						updateDate(-1);
+					}
+					else if (currentPage > 1){
+						updateDate(1);
+					}
+
+					pageIndicator.setCurrentItem(CURRENT_POSITION, false);
+				}
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				// TODO Auto-generated method stub
+				
+
+			}
+
+			@Override
+			public void onPageSelected(int position) {
+				// TODO Auto-generated method stub
+				currentPage = position;
+			}
+
+		});
+	}
+
+	protected void updateDate(int changedDays) {
+		// TODO Auto-generated method stub
+		
+		// Calculate current date
+		calChanging.add(Calendar.DAY_OF_MONTH, changedDays);
+		
+		currentDay = calChanging.get(Calendar.DATE);
+		currentMonth = calChanging.get(Calendar.MONTH);
+		currentYear = calChanging.get(Calendar.YEAR);
+		
+		currentDate = currentDay + " " + monthsName[currentMonth] +
+				", " + currentYear;
+		
+		// Calculate previous date
+		calChanging.add(Calendar.DAY_OF_MONTH, -1);
+		
+		previousDay = calChanging.get(Calendar.DATE);
+		previousMonth = calChanging.get(Calendar.MONTH);
+		previousYear = calChanging.get(Calendar.YEAR);
+		
+		previousDate = previousDay + " " + monthsName[previousMonth] +
+				", " + previousYear;
+		
+		// Calculate next date
+		calChanging.add(Calendar.DAY_OF_MONTH, +2);
+		
+		nextDay = calChanging.get(Calendar.DATE);
+		nextMonth = calChanging.get(Calendar.MONTH);
+		nextYear = calChanging.get(Calendar.YEAR);
+		
+		nextDate = nextDay + " " + monthsName[nextMonth] + 
+				", " + nextYear;
+		
+		// Reset calChanging to current day
+		calChanging.add(Calendar.DAY_OF_MONTH, -1);
 	}
 
 	@Override
@@ -83,9 +196,9 @@ public class DefaultView extends FragmentActivity {
 	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
 	 * one of the sections/tabs/pages.
 	 */
-	public class SectionsPagerAdapter extends FragmentPagerAdapter {
+	public class CalendarPagerAdapter extends FragmentStatePagerAdapter {
 
-		public SectionsPagerAdapter(FragmentManager fm) {
+		public CalendarPagerAdapter(FragmentManager fm) {
 			super(fm);
 		}
 
@@ -104,19 +217,20 @@ public class DefaultView extends FragmentActivity {
 		@Override
 		public int getCount() {
 			// Show 3 total pages.
-			return 3;
+			return PAGE_NUMBER;
 		}
 
 		@Override
 		public CharSequence getPageTitle(int position) {
 			Locale l = Locale.getDefault();
+			
 			switch (position) {
 			case 0:
-				return getString(R.string.testing_date).toUpperCase(l);
+				return previousDate.toUpperCase(l);
 			case 1:
-				return getString(R.string.testing_date).toUpperCase(l);
+				return currentDate.toUpperCase(l); 
 			case 2:
-				return getString(R.string.testing_date).toUpperCase(l);
+				return nextDate.toUpperCase(l); 
 			}
 			return null;
 		}
@@ -144,5 +258,4 @@ public class DefaultView extends FragmentActivity {
 			return rootView;
 		}
 	}
-
 }
