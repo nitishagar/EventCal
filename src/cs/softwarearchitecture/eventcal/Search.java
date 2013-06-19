6d7b5185efcf98600023f9a7185bfd97ddf5a94a
Nitish Agarwal <nitishagar@gmail.com>
2013-06-19 12:52:21 -0400

    Search functionality completed"

--- src/cs/softwarearchitecture/eventcal/Search.java
+++ src/cs/softwarearchitecture/eventcal/Search.java
@@ -13,6 +13,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import cs.softwarearchitecture.eventcal.contentprovider.DBEventsContentProvider;
 import cs.softwarearchitecture.eventcal.database.DBSQLiteHelper;
+import cs.softwarearchitecture.eventcal.modify.EditEvent;
 
 public class Search extends ListActivity { 
 	
@@ -31,7 +32,50 @@ public class Search extends ListActivity {
 
 	public void onListItemClick(ListView l, 
 			View v, int position, long id) { 
-		// call detail activity for clicked entry 
+		String selectedTitle = mSearchResults.get(position);
+		
+		// Get all the details about that event
+		String selection = DBSQLiteHelper.COLUMN_TITLE + " LIKE ?";
+		String[] selectionArgs = new String[] { selectedTitle };
+		
+//		Log.d(DefaultView.TAG, queryStr + columnsReturn[0] + selectionArgs[0]);
+		Cursor cursor = 
+				getContentResolver().query(
+						DBEventsContentProvider.CONTENT_URI, null, 
+						selection, selectionArgs, DBSQLiteHelper.COLUMN_START_DATE + " ASC");
+		
+		// Intent for showing event details
+		Intent editEventIntent = 
+				new Intent(this, EditEvent.class);
+		
+		if (cursor.moveToFirst()) {
+			Log.d(DefaultView.TAG, "loading selected events");
+			while(!cursor.isAfterLast()){
+				int _id = cursor.getInt(cursor.getColumnIndex(DBSQLiteHelper.COLUMN_ID));
+                String title = cursor.getString(cursor.getColumnIndex(DBSQLiteHelper.COLUMN_TITLE));
+				String start_time = cursor.getString(cursor.getColumnIndex(DBSQLiteHelper.COLUMN_START_TIME));
+				String end_time = cursor.getString(cursor.getColumnIndex(DBSQLiteHelper.COLUMN_END_TIME));
+				String date = cursor.getString(cursor.getColumnIndex(DBSQLiteHelper.COLUMN_START_DATE));
+				String group = cursor.getString(cursor.getColumnIndex(DBSQLiteHelper.COLUMN_TABLE));
+				int reminder = cursor.getInt(cursor.getColumnIndex(DBSQLiteHelper.COLUMN_REMINDER_TIME));
+				
+				editEventIntent.putExtra("title", title);
+				editEventIntent.putExtra("start_time", start_time);
+				editEventIntent.putExtra("end_time", end_time);
+				editEventIntent.putExtra("date", date);
+				editEventIntent.putExtra("reminder", reminder);
+				editEventIntent.putExtra("group", group);
+				editEventIntent.putExtra("id", _id);
+				
+				cursor.moveToNext();
+			}
+			Log.d(DefaultView.TAG, mSearchResults.toString());
+		}
+		
+		// Start detail activity aka edit event
+		editEventIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		startActivity(editEventIntent);
+		
 	} 
 
 	private void handleIntent(Intent intent) { 
