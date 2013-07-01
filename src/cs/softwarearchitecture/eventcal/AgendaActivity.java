75c97336a0e0c52668acbbe0832fba4792c5422c
Nitish Agarwal <nitishagar@gmail.com>
2013-07-01 03:57:20 -0400

    Very rough implementation of agenda view

--- src/cs/softwarearchitecture/eventcal/AgendaActivity.java
+++ src/cs/softwarearchitecture/eventcal/AgendaActivity.java
@@ -1,25 +1,70 @@
 package cs.softwarearchitecture.eventcal;
 
-import android.os.Bundle;
 import android.app.ActionBar;
-import android.app.Activity;
 import android.app.ActionBar.OnNavigationListener;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.ContextThemeWrapper;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.AdapterViewFlipper;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.SpinnerAdapter;
+import cs.softwarearchitecture.eventcal.contentprovider.DBEventsContentProvider;
+import cs.softwarearchitecture.eventcal.database.DBSQLiteHelper;
 
-public class AgendaActivity extends DefaultView {
+public class AgendaActivity extends DefaultView implements LoaderManager.LoaderCallbacks<Cursor>  {
 
 	public static int AGENDA_VIEW = 2;
+	
+	// Identifies a particular Loader being used in this component
+    private static final int URL_LOADER = 0;
+
+    String[] mFromColumns = { DBSQLiteHelper.COLUMN_TITLE };
+    int[] mToFields = { R.id.event_title };
+
+	AgendaCursorAdapter mAdapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_agenda);
+		
+		/*
+		 * Initializes the CursorLoader. The URL_LOADER value is eventually passed
+		 * to onCreateLoader().
+		 */
+		getLoaderManager().initLoader(URL_LOADER, null, this);
+
+	    // Gets a handle to a List View
+		try {
+			ListView mListView = (ListView) findViewById(R.id.event_list);
+			
+			mAdapter = new AgendaCursorAdapter(this, null);
+//					new SimpleCursorAdapter(
+//							this,                // Current context
+//							R.layout.agenda_item,  // Layout for a single row
+//							null,                // No Cursor yet
+//							mFromColumns,        // Cursor columns to use
+//							mToFields,           // Layout fields to use
+//							0                    // No flags
+//							);
+			
+			// Sets the adapter for the view
+			mListView.setAdapter(mAdapter);
+			
+//			mListView.setOnItemClickListener(eventDetail);
+		}
+		catch(Exception e){
+			Log.e(DefaultView.TAG, "Exception caught: " + e.getMessage());
+		}
+
 	}
 
 	@Override
@@ -76,6 +121,50 @@ public class AgendaActivity extends DefaultView {
 
 		actionBar.setListNavigationCallbacks(mSpinnerAdapter, mOnNavigationListener);
 		
+	}
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int loaderID, Bundle bundle) {
+		/*
+	     * Takes action based on the ID of the Loader that's being created
+	     */
+	    switch (loaderID) {
+	        case URL_LOADER:
+	            // Returns a new CursorLoader
+	        	String[] projection = { DBSQLiteHelper.COLUMN_ID, DBSQLiteHelper.COLUMN_TITLE, DBSQLiteHelper.COLUMN_TABLE };
+	        	
+	            return new CursorLoader(
+	                        this,						    		     // Parent activity context
+	                        DBEventsContentProvider.CONTENT_URI,        // Table to query
+	                        projection,                                // Projection to return
+	                        null,            						  // No selection clause
+	                        null,            									  // No selection arguments
+	                        DBSQLiteHelper.COLUMN_START_DATE + " ASC, " 
+	                        + DBSQLiteHelper.COLUMN_START_TIME + " ASC"			  // Default sort order
+	        );
+	        default:
+	            // An invalid id was passed in
+	        	Log.d(DefaultView.TAG, "Invalid ID!");
+	            return null;
+	    }
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+		/*
+	     * Moves the query results into the adapter, causing the
+	     * ListView fronting this adapter to re-display
+	     */
+	    mAdapter.changeCursor(cursor);
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+		 /*
+	     * Clears out the adapter's reference to the Cursor.
+	     * This prevents memory leaks.
+	     */
+	    mAdapter.changeCursor(null);
 	}
 
 }
---
+++ src/cs/softwarearchitecture/eventcal/AgendaCursorAdapter.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package cs.softwarearchitecture.eventcal;
+
+import cs.softwarearchitecture.eventcal.database.DBSQLiteHelper;
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * @author nitishagarwal
+ *
+ */
+public class AgendaCursorAdapter extends CursorAdapter {
+
+	private LayoutInflater mLayoutInflater;
+    private Context mContext;
+    
+    public AgendaCursorAdapter(Context context, Cursor c) {
+        super(context, c);
+        mContext = context;
+        mLayoutInflater = LayoutInflater.from(context); 
+    }
+    
+	/* (non-Javadoc)
+	 * @see android.support.v4.widget.CursorAdapter#bindView(android.view.View, android.content.Context, android.database.Cursor)
+	 */
+	@Override
+	public void bindView(View view, Context context, Cursor cursor) {
+		TextView event_title = (TextView)view.findViewById(R.id.event_title);
+		ImageView event_image = (ImageView)view.findViewById(R.id.list_image);
+		event_title.setText(cursor.getString(
+				cursor.getColumnIndex(DBSQLiteHelper.COLUMN_TITLE)));
+		
+		String image_resource_type = cursor.getString(
+				cursor.getColumnIndex(DBSQLiteHelper.COLUMN_TABLE));
+		
+		if (image_resource_type.equals("PERSONAL")) {
+			// TODO create a personal image
+			event_image.setImageResource(R.drawable.ic_action_google_event);
+		}
+		
+		if (image_resource_type.equals("FACEBOOK")) {
+			event_image.setImageResource(R.drawable.ic_action_facebook_event);
+		}
+		
+		if (image_resource_type.equals("EVENTBRITE")) {
+			event_image.setImageResource(R.drawable.ic_action_eventbrite_event);
+		}
+		
+		if (image_resource_type.equals("UW")) {
+			event_image.setImageResource(R.drawable.ic_action_uw_event);
+		}
+		
+		if (image_resource_type.equals("GOOGLE")) {
+			event_image.setImageResource(R.drawable.ic_action_google_event);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see android.support.v4.widget.CursorAdapter#newView(android.content.Context, android.database.Cursor, android.view.ViewGroup)
+	 */
+	@Override
+	public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+		LayoutInflater inflater = LayoutInflater.from(context);
+		View view = inflater.inflate(R.layout.agenda_item, viewGroup, false);
+		bindView(view, context, cursor);
+		return view;
+	}
+
+}
